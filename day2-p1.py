b7cf5be3bd4804fdfe292b5ce06753daaf3ca2d4
shadezer0 <ashleyjames2800@gmail.com>
2022-09-07 11:09:28 +0530

    Add comments to d2p1

--- day2-p1.py
+++ day2-p1.py
@@ -8,8 +8,10 @@ vertical_pos = 0
 input_file = "inputs/day2-input"
 
 with open(input_file, encoding="utf-8") as f:
+    # change input from "forward 5\n down 3\n" to [(forward, 5), (down, 3)]
     course = [(x.split()[0], x.split()[1]) for x in f]
 
+    # loop over each tuple in the course
     for direction, value in course:
         match direction:
             case "forward":
