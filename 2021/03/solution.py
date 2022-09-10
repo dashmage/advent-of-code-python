62adbab721a7775e0027b7fbad0453a372e7e9c3
shadezer0 <ashleyjames2800@gmail.com>
2022-09-10 16:49:20 +0530

    Finish d3p2

--- 2021/03/solution.py
+++ 2021/03/solution.py
@@ -32,5 +32,90 @@ def part1(binary_nums):
     return int(gamma, 2) * int(epsilon, 2)
 
 
-def part2(data):
-    pass
+def part2(binary_nums):
+    len_bin_num = len(binary_nums[0])
+
+    co2_rating = 0
+    oxygen_rating = 0
+
+    # copy of binary nums tracking most common values
+    binary_nums_more = binary_nums.copy()
+    # copy of binary nums tracking least common values
+    binary_nums_less = binary_nums.copy()
+
+    # loop over all bits in binary num
+    for bit_index in range(len_bin_num):
+        # list to hold count for binary nums with most common bits
+        count_zero_more = [0 for _ in range(len_bin_num)]
+        count_one_more = [0 for _ in range(len_bin_num)]
+
+        # list to hold count for binary nums with least common bits
+        count_zero_less = [0 for _ in range(len_bin_num)]
+        count_one_less = [0 for _ in range(len_bin_num)]
+
+        # loop over all nums to figure out most common bit in each position
+        # most common bits
+        for num in binary_nums_more:
+            for j, bit in enumerate(num):
+                if bit == "1":
+                    count_one_more[j] += 1
+                else:
+                    count_zero_more[j] += 1
+
+        # least common bits
+        for num in binary_nums_less:
+            for j, bit in enumerate(num):
+                if bit == "1":
+                    count_one_less[j] += 1
+                else:
+                    count_zero_less[j] += 1
+
+        # keep only binary nums with most common bit in bit_index position
+        # if 0s > 1s in bit_index position, delete nums with 1 in that position
+        i = 0
+        if count_zero_more[bit_index] > count_one_more[bit_index]:
+            # loop over all binary nums
+            while i < len(binary_nums_more):
+                if binary_nums_more[i][bit_index] == "1":
+                    del binary_nums_more[i]
+                    i -= 1
+                i += 1
+
+        else:
+            while i < len(binary_nums_more):
+                if binary_nums_more[i][bit_index] == "0":
+                    del binary_nums_more[i]
+                    i -= 1
+                i += 1
+
+        # keep only binary nums with least common bit in bit_index position
+        # if 0s < 1s in bit_index position, delete nums with 1 in that position
+        i = 0
+        if count_zero_less[bit_index] <= count_one_less[bit_index]:
+            # loop over all binary nums
+            while i < len(binary_nums_less):
+                if binary_nums_less[i][bit_index] == "1":
+                    del binary_nums_less[i]
+                    i -= 1
+                i += 1
+
+        else:
+            while i < len(binary_nums_less):
+                if binary_nums_less[i][bit_index] == "0":
+                    del binary_nums_less[i]
+                    i -= 1
+                i += 1
+
+        # if there's just one num remaining for binary nums tracking most common values
+        if len(binary_nums_more) == 1:
+            oxygen_rating = binary_nums_more[0]
+
+        # if there's just one num remaining for binary nums tracking least common values
+        if len(binary_nums_less) == 1:
+            co2_rating = binary_nums_less[0]
+
+        # break out of loop once both oxygen and co2 rating are non-zero
+        if oxygen_rating and co2_rating:
+            break
+
+    return int(oxygen_rating, 2) * int(co2_rating, 2)
---
+++ 2021/test.py
@@ -0,0 +1,6 @@
+day = "03"
+input_file_name = f"2021/{day}/example_input.txt"
+
+with open(input_file_name, encoding="utf-8") as f:
+    lines = f.read().split("\n")
+    print(lines)
