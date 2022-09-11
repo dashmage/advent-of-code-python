6f485d6ba74bbc08225d0184cdd6d23fb80f4c04
shadezer0 <ashleyjames2800@gmail.com>
2022-09-11 23:33:55 +0530

    Finish d6p2

--- 2021/06/solution.py
+++ 2021/06/solution.py
@@ -9,18 +9,32 @@ def parse_input(lines):
 
 def part1(fish_timers):
     days = 80
-    # day_index = 0
-    # print(f"Initial state: {fish_timers}")
-    for i in range(days):
+    for day_index in range(1, days + 1):
         for i, age in enumerate(fish_timers):
-            fish_timers[i] -= 1
             if age == 0:
                 fish_timers.append(9)
-                fish_timers[i] = 6
-        # day_index += 1
-        # print(f"After {day_index} days: {fish_timers}")
-    return len(fish_timers)
+                fish_timers[i] = 7
+        fish_timers = list(map(lambda x: x - 1, fish_timers))
+        # print(f"After {day_index} days: {sorted(fish_timers)}, {len(fish_timers)}")
+    return len(list(fish_timers))
 
 
-def part2(data):
-    pass
+def part2(fish_timers):
+    days = 256
+    count_nums = [fish_timers.count(x) for x in range(9)]
+    fish_timers_d = dict(zip(range(9), count_nums))
+    zero_extra = 0
+    for day_index in range(days):
+        # print(day_index + 1, fish_timers_d)
+        for i in range(8):
+            fish_timers_d[i] = fish_timers_d[i + 1]
+            if i == 6:
+                fish_timers_d[6] += zero_extra
+        if zero_extra:
+            fish_timers_d[8] = zero_extra
+            zero_extra = 0
+        else:
+            fish_timers_d[8] = 0
+        # print(day_index + 1, fish_timers_d)
+        zero_extra = fish_timers_d[0]
+    return sum(fish_timers_d.values())
