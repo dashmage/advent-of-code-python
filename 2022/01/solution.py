85a084bc4a15e53789ddc4bf88d4a7107496414e
Ashley James <ashleyjames2800@gmail.com>
2022-12-25 22:55:14 +0530

    Finish 2022/01

--- 2022/01/solution.py
+++ 2022/01/solution.py
@@ -3,10 +3,26 @@
 ## day 01
 
 def parse_input(lines):
-    pass
+    """
+    Returns a list containing the calories carried by each elf within sublists
+    eg: [[1000, 2000], [3000], [4000, 5000]]
+    """
+    output = [[]]
+    n = 0
+    for cal in lines:
+        if cal != '':
+            output[n].append(int(cal))
+        else:
+            n += 1
+            output.append([])
+    return output
 
-def part1(data):
-    pass
 
-def part2(data):
-    pass
+def part1(calories):
+    # list containing the sum of calories of the food carried by each elf
+    sum_calories = [sum(c) for c in calories]
+    return max(sum_calories)
+
+def part2(calories):
+    sum_calories = [sum(c) for c in calories]
+    return sum(sorted(sum_calories, reverse=True)[:3])
